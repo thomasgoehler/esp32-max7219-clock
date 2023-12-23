7322eb1e830fd454dddbdf744ffd42e066875741
Thomas Goehler <51946772+thomasgoehler@users.noreply.github.com>
2023-12-23 23:14:27 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /**********  User Config Setting   ******************************/
-#define SET_SSID "KeDoHome2"
-#define SET_PASSWORD "67145647306486273038"
+#define SET_SSID "YOUR_SSID"
+#define SET_PASSWORD "YOUR_PASSWORD"
 #define SET_TIMEZONE_SECONDS 3600
 /***************************************************************/
 
