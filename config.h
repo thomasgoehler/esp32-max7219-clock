941b6ea2625956cb207c4f13092d89b94233a547
Thomas Goehler <51946772+thomasgoehler@users.noreply.github.com>
2023-12-23 23:26:16 +0100

    Delete config.h

--- config.h
+++
@@ -1,6 +0,0 @@
-/**********  User Config Setting   ******************************/
-#define SET_SSID "YOUR_SSID"
-#define SET_PASSWORD "YOUR_PASSWORD"
-#define SET_TIMEZONE_SECONDS 3600
-/***************************************************************/
-
