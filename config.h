9cad589d23ff7cc469fabda0190d94ca67b3ad25
Thomas Göhler <th.goehler@googlemail.com>
2023-12-23 23:10:48 +0100

    add real credentials

--- config.h
+++ config.h
@@ -1,3 +1,6 @@
-#define SET_SSID "YOUR WIFI SSID"
-#define SET_PASSWORD "YOUR PASSWORD"
-#define SET_TIMEZONE_SECONDS 3600    // GMT plus or minus in seconds to match with your timezone
+/**********  User Config Setting   ******************************/
+#define SET_SSID "KeDoHome2"
+#define SET_PASSWORD "67145647306486273038"
+#define SET_TIMEZONE_SECONDS 3600
+/***************************************************************/
+
